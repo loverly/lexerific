e296428a603fa82b6f12d165bfdb37b44a95d36e
Brandon Eum <brandon@lover.ly>
2015-05-11 13:32:53 -0400

    Added the ability to specify steps as "inverted"
    * Inverted steps match everything EXCEPT the specified pattern
    * Limitations right now include only being able to specify one
      inversion at each tree level

--- examples/simple.js
+++ examples/simple.js
@@ -7,7 +7,7 @@ lexer.setDefaultLexeme({
   partialMatchAttributeValue: function (history, input) {
     var attrVal = '';
     history.shift(); // Remove the root node
-
+console.log('PARTIAL')
     // Add each node from the root to where we are
     history.forEach(function (state) {
       attrVal += state.input.token;
@@ -28,6 +28,25 @@ lexer.addLexeme({
     {token: 'l', optional: true, repeat: true},
     {token: 'a', optional: true, repeat: true},
     {token: 'x', repeat: true}
+  ],
+  attributeValue: function (history, input) {
+    var attr = '';
+
+    history.shift(); // Remove root node
+    history.forEach(function (visit) {
+      attr += visit.input.token;
+    });
+
+    return attr;
+  }
+});
+
+lexer.addLexeme({
+  token: 'coolbeans',
+  pattern: [
+    {token: 'z'},
+    {token: 'e', inverted: true, repeat: true},
+    {token: 'e'}
   ],
   attributeValue: function (history, input) {
     var attr = '';
@@ -65,5 +84,5 @@ lexer.on('data', function (data) {
   console.log('TOKEN:', data);
 });
 
-lexer.write('bx bxx bex bexx bellxx bellaaxx beaaxx besssxxx');
+lexer.write('zavnwoie');
 lexer.end();
